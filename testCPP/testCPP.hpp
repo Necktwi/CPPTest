97be9cc7f46b27949f6571c057791e9cebc87e3d
Gowtham Kudupudi <gowtham@ferryfair.com>
2018-06-18 20:02:28 +0530

    ClassNStruct

--- testCPP/testCPP.hpp
+++ testCPP/testCPP.hpp
@@ -9,6 +9,8 @@
 #ifndef testCPP_
 #define testCPP_
 
+#include <iostream>
+
 /* The classes below are exported */
 #pragma GCC visibility push(default)
 
@@ -22,6 +24,104 @@ unsigned long factorial (unsigned long n);
 unsigned long factorial_by_loop (unsigned short n);
 unsigned int fibonacci (unsigned int n);
 unsigned int fibonacci_by_loop (unsigned int n);
+
+void floating_point1 ();
+void floating_point2 ();
+
+class ClassEG_ {
+   int      IntMem = 0;
+   float    FloatMem = 0.0;
+   uint16_t ShortMem = 0.0;
+   char     CharMem = 30;
+};
+
+struct StructEG_ {
+   int      IntMem = 0;
+   float    FloatMem = 0.0;
+   uint16_t ShortMem = 0.0;
+   char     CharMem = 30;
+};
+
+unsigned long factorial (unsigned long n) {
+   if (n == 1) return 1;
+   return (n * factorial(n - 1));
+}
+
+unsigned long factorial_by_loop (unsigned short n) {
+   unsigned long factorial = n;
+   while (n>1) {
+      factorial *= --n;
+   }
+   return factorial;
+}
+
+unsigned int fibonacci (unsigned int n) {
+   if (n <= 2) {
+      return 1;
+   } else {
+      return fibonacci(n-1) + fibonacci(n-2);
+   }
+}
+
+unsigned int fibonacci_by_loop (unsigned int n) {
+   unsigned int n0 = 1;
+   unsigned int n1 = 1;
+   unsigned int n2 = 1;
+   for (;n>2;--n) {
+      n2 = n1;
+      n1 = n0;
+      n0 = n1 + n2;
+   }
+   return n0;
+}
+
+void floating_point1 () {
+   std::size_t length = 4;
+   int* d = new int[length];
+
+   for (std::size_t i = 0; i < length; )
+      d[i] = ++i;
+   for (std::size_t i = length - 1; i >= 0; i--) 
+      std::cout << d[length - 1] << " ";
+   const int N = 16;
+   const float a[N] = { 3.1, 3.2, 3.3, 3.4, 5.5, 3.1, 3.2, 3.3,
+      3.4, 5.5, 3.1, 3.2, 3.3, 3.4, 5.5, 6.6 };
+   const float b[N] = { 3.13, 3.23, 3.33, 3.43, 5.53, 3.13, 3.23,
+      3.33, 3.433, 5.533, 3.133, 3.233, 3.333, 3.433, 5.533, 6.633 };
+   float c[N];
+   for (int i=0; i<N; ++i) {
+      c[i] = a[i];
+   }
+   for (int i=0; i<9999999; ++i) {
+      for (int j=0; j<N; ++j) {
+         c[j] *= a[j];
+         c[j] /= b[j];
+         c[j] = c[j] + 0.2f;
+         c[j] = c[j] - 0.2f;
+      }
+   }
+}
+
+void floating_point2 () {
+   const int N = 16;
+   const float a[N] = { 3.1, 3.2, 3.3, 3.4, 5.5, 3.1, 3.2, 3.3,
+      3.4, 5.5, 3.1, 3.2, 3.3, 3.4, 5.5, 6.6 };
+   const float b[N] = { 3.13, 3.23, 3.33, 3.43, 5.53, 3.13, 3.23,
+      3.33, 3.433, 5.533, 3.133, 3.233, 3.333, 3.433, 5.533, 6.633 };
+   float c[N];
+   for (int i=0; i<N; ++i) {
+      c[i] = a[i];
+   }
+   
+   for (int i=0; i<9999999; ++i) {
+      for (int j=0; j<N; ++j) {
+         c[j] *= a[j];
+         c[j] /= b[j];
+         c[j] = c[j] + 0;
+         c[j] = c[j] - 0;
+      }
+   }
+}
 
 #pragma GCC visibility pop
 #endif
--- testCPPTests/main.cpp
+++ testCPPTests/main.cpp
@@ -11,72 +11,76 @@
 #include <FerryTimeStamp.h>
 #include <mystdlib.h>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
    std::cout << "%SUITE_STARTED% testCPP" << std::endl << std::endl;
    
-   FerryTimeStamp ftsStart;
-   FerryTimeStamp ftsEnd;
-   FerryTimeStamp ftsDiff;
-   FerryTimeStamp ftsSuiteStart;
-   FerryTimeStamp ftsSuiteEnd;
-   ftsSuiteStart.Update();
+   FerryTimeStamp TestStart;
+   FerryTimeStamp TestEnd;
+   FerryTimeStamp Diff;
+   FerryTimeStamp SuiteStart;
+   FerryTimeStamp SuiteEnd;
+   SuiteStart.Update();
    
+   unsigned long f = 0;
    std::cout << "%TEST_STARTED% factorial" << std::endl;
-   ftsStart.Update();
-   std::cout << factorial(15) << std::endl;
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " factorial" << std::endl;
+   TestStart.Update(); {
+      f = factorial(15);
+   }; TestEnd.Update();
+   std::cout << "factorial(15): " << f << std::endl;
+   Diff = TestEnd - TestStart;
+   std::cout << "%TEST_FINISHED% time: " << Diff << " factorial" << std::endl;
    std::cout << std::endl;
    
    std::cout << "%TEST_STARTED% factorial_by_loop" << std::endl;
-   ftsStart.Update();
-   std::cout << factorial_by_loop(15) << std::endl;
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " factorial_by_loop" <<
+   TestStart.Update(); {
+      f = factorial_by_loop(15);
+   }; TestEnd.Update();
+   std::cout << "factorial_by_loop(15): " << f << std::endl;
+   Diff = TestEnd - TestStart;
+   std::cout << "%TEST_FINISHED% time: " << Diff << " factorial_by_loop" <<
       std::endl;
    std::cout << std::endl;
    
-   unsigned int f = 0;
    std::cout << "%TEST_STARTED% fibonacci" << std::endl;
-   ftsStart.Update();
-   f = fibonacci(15);
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " fibonacci" << std::endl;
-   std::cout << std::endl;
-   
-   std::cout << "%TEST_STARTED% print fibonacci" << std::endl;
-   ftsStart.Update();
-   std::cout << f << std::endl;
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " print fibonacci" <<
-      std::endl;
+   TestStart.Update(); {
+      f = fibonacci(15);
+   }; TestEnd.Update();
+   cout << "fibonacci(15): " << f << endl;
+   Diff = TestEnd - TestStart;
+   std::cout << "%TEST_FINISHED% time: " << Diff << " fibonacci" << std::endl;
    std::cout << std::endl;
    
    std::cout << "%TEST_STARTED% fibonacci_by_loop" << std::endl;
-   ftsStart.Update();
-   f = fibonacci_by_loop(15);
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " fibonacci_by_loop" <<
+   TestStart.Update(); {
+      f = fibonacci_by_loop(15);
+   }; TestEnd.Update();
+   cout << "fibonacci_by_loop(15): " << f << endl;
+   Diff = TestEnd - TestStart;
+   std::cout << "%TEST_FINISHED% time: " << Diff << " fibonacci_by_loop" <<
       std::endl;
    std::cout << std::endl;
    
-   std::cout << "%TEST_STARTED% print fibonacci_by_loop" << std::endl;
-   ftsStart.Update();
-   std::cout << f << std::endl;
-   ftsEnd.Update();
-   ftsDiff = ftsEnd - ftsStart;
-   std::cout << "%TEST_FINISHED% time=" << ftsDiff << " print fibonacci_by_loop"
+   std::cout << "%TEST_STARTED% print ClassNStruct" << std::endl;
+   TestStart.Update(); {
+      cout << "{" << endl;
+      cout << "\tInt size: "    << sizeof(int)       << endl;
+      cout << "\tFloat size: "  << sizeof(float)     << endl;
+      cout << "\tuint16 size: " << sizeof(uint16_t)  << endl;
+      cout << "\tChar size: "   << sizeof(char)      << endl;
+      cout << "}" << endl;
+      cout << "Class size: "  << sizeof(ClassEG_)  << endl;
+      cout << "Struct size: " << sizeof(StructEG_) << endl;
+   }; TestEnd.Update();
+   Diff = TestEnd - TestStart;
+   std::cout << "%TEST_FINISHED% time: " << Diff << " print ClassNStruct"
       << std::endl;
    std::cout << std::endl;
    
-   ftsSuiteEnd.Update();
-   ftsDiff = ftsSuiteEnd - ftsSuiteStart;
-   std::cout << "%SUITE_FINISHED% time=" << ftsDiff << std::endl;
+   SuiteEnd.Update();
+   Diff = SuiteEnd - SuiteStart;
+   std::cout << "%SUITE_FINISHED% time: " << Diff << std::endl;
    
    return (EXIT_SUCCESS);
 }
